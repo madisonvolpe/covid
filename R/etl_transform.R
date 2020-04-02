c17db94ced90029a9b243e33fe5709f4ebeb8401
Madison Volpe <mv1617@nyu.edu>
2020-04-02 16:34:22 -0400

    progress on transform script, tmrw test

--- R/etl_transform.R
+++ R/etl_transform.R
@@ -13,22 +13,38 @@
 
 etl_transform.etl_covid <- function(obj, ...){
 
-  files <- list.files(attr(obj, "raw_dir"), "\\.csv", full.names = T)
+  # Reading in
 
-  covid_dfs <- purrr::map(files, readr::read_csv)
+  # Read in raw files from raw_dir, raw_dir is either specified directory in initial etl call
+  # or from temp directory in initial etl call
+  files <- list.files(attr(obj, "raw_dir"), "\\.csv", full.names = T)
+  covid_dfs <- purrr::map(files, readr::read_csv) %>% set_names(map_chr(files, ~str_extract(.x, "\\d{4}\\-\\d{2}\\-\\d{2}")))
+
+  # Cleaning
+
+  # Make all column names in dfs match
   covid_dfs <- purrr::map(covid_dfs,standardize_names)
 
-  print(covid_dfs)
+  # Cleaning dataframes for easy loading to SQL db
+  covid_dfs <- purrr::map(covid_dfs, ~purrr::map_df(.x, ~stringr::str_replace_all(., "'", "")))
+  covid_dfs <- purrr::map(covid_dfs, cleaning_covid_datasets)
+
+  # Saving transformed files to load folder in directory or temp directory
+  covid_dfs %>% names(.) %>% purrr::map(~readr::write_csv(covid_dfs[[.]], paste0(attr(obj, "load_dir"),"/",.,".csv")))
+
+  return(covid_dfs)
+
 }
 
-# Internal function
-#' All dataframes in list should be standardized
-#' param takes a df (from list) and applies cleaning functions
+# Internal function 1
+
+#' All columns in every dataframe in  the list should have the same column names
+#' @param df takes a df and makes column names standardized
 #' @noRd
 
 standardize_names <- function(df){
 
-  # take out column names and apply operations to them
+  # Take out column names and apply operations to them
   names_df <- names(df)
 
   names_df <- tolower(names_df)
@@ -44,3 +60,24 @@ standardize_names <- function(df){
 
   return(df)
 }
+
+# Internal function 2
+
+#' Cleaning all covid dfs to make them acceptable for loading to a SQL database
+#'  @param df takes a df and cleans it
+#'  cleaning includes removing apostrophes
+#'  converting dates
+#'  converting numeric
+#'  @noRd
+
+cleaning_covid_datasets <- function(df){
+
+  df <- df %>%
+    dplyr::mutate_at(vars(starts_with("last")), list(~stringr::str_replace_all(., "T", " "))) %>%
+    dplyr::mutate_at(vars(starts_with("last")),
+                     list(~as.character(lubridate::parse_date_time(.,orders = c('mdy_hm', 'ymd_hms'))))) %>%
+    dplyr::mutate_at(vars(matches("^conf|^reco|^deat|^acti")), as.numeric)
+
+  return(df)
+}
+
